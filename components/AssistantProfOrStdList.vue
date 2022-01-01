e167df1b00a1594b6fa578f660dfe50e1ce57cfc
killing-joke <pooyafekri79@gmail.com>
2022-01-01 16:43:25 +0330

    add course page and prof and std

---
+++ components/AssistantProfOrStdList.vue
@@ -0,0 +1,208 @@
+<template>
+  <v-container>
+    <v-card width="100%" justify="center" style="background: #dce4eb">
+      <v-card-title class="text-h5 lighten-2 ticket_title_background mb-5">
+        {{ title }}
+      </v-card-title>
+      <v-row justify="center">
+        <v-col cols="11">
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            sort-by="rowNum"
+            class="elevation-1 mb-3"
+            item-key="item.name"
+          >
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-row>
+                <v-icon
+                  color="black"
+                  small
+                  class="mx-2"
+                  @click="editCourseProf = true"
+                >
+                  mdi-pencil
+                </v-icon>
+
+                <v-icon color="black" small @click="deleteItem(item)">
+                  mdi-delete
+                </v-icon>
+              </v-row>
+            </template>
+          </v-data-table>
+
+          <v-row>
+            <v-col>
+              <AddCourseToElementary
+                :visible="AddCourseToElementary"
+                :masterOrBachelor="type === 'master' ? true : false"
+                @close="AddCourseToElementary = false"
+              />
+              <EditCourseProf
+                :visible="editCourseProf"
+                :item="{}"
+                @close="editCourseProf = false"
+              />
+            </v-col>
+          </v-row>
+          <v-row justify="end">
+            <v-col>
+              <v-btn icon class="add_ticket_btn">
+                <v-icon
+                  circle
+                  color="white"
+                  @click="AddCourseToElementary = true"
+                >
+                  mdi-plus
+                </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, default: () => '' },
+    type: { type: String, default: () => '' },
+  },
+  data() {
+    return {
+      stdHeadrs: [
+        { text: 'ردیف', align: 'start', value: 'rowNum' },
+        { text: 'نام دانشجو', value: 'stdName', sortable: false },
+        { text: 'شماره دانشجویی', value: 'stdNum', sortable: false },
+        { text: 'سال ورود', value: 'entryYear', sortable: false },
+        { text: 'مقطع', value: 'section', sortable: false },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      profHeaders: [
+        { text: 'ردیف', align: 'start', value: 'rowNum' },
+        { text: 'نام استاد', value: 'profName', sortable: false },
+        { text: 'ایمیل', value: 'email', sortable: false },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      // studHeader: [
+      //     { text: 'ردیف', align: 'start', value: 'rowNum', },
+      //     { text: 'نوع تیکت', value: 'ticketType', sortable: false  },
+      //     { text: 'تاریخ', value: 'date',  },
+      //     // { text: 'توضیحات', value: 'caption', sortable: false  },
+      //     { text: 'وضعیت', value: 'status', sortable: false },
+      //     { text: 'Actions', value: 'actions', sortable: false }
+      // ],
+
+      selects: [],
+      bachelorItems: [],
+      masterItems: [],
+      editCourseProf: false,
+      AddCourseToElementary: false,
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.getters['auth/user']
+    },
+
+    role() {
+      return this.user.role.name_role
+    },
+
+    items() {
+      return this.type === 'std'
+        ? this.$store.getters['assistant/students']
+        : this.$store.getters['assistant/profs']
+      // console.log(this.type);
+      // return this.$store.getters['assistant/students']
+    },
+    headers() {
+      return this.type === 'std' ? this.stdHeadrs : this.profHeaders
+    },
+  },
+
+  async mounted() {
+  },
+  updated() {},
+
+  methods: {
+
+    deleteItem(item){
+      if (this.type === 'std'){
+        
+      }else{
+
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+.ticket_title_background {
+  background: #3f505e;
+  /* background: #618AAB;     */
+  color: white;
+  display: flex;
+  justify-content: center;
+}
+
+.terminate_ticket {
+  background: #e28743;
+  color: white;
+  width: 128px;
+}
+
+.cancel_ticket {
+  background: #84a7c4;
+  color: white;
+  width: 128px;
+}
+
+.ticket_background {
+  background: #dce4eb;
+}
+
+.closeWhite {
+  color: white;
+  background: white;
+}
+
+.comments_color {
+  color: #3f505e;
+  font-size: 24px;
+}
+
+.add_ticket_btn {
+  background: #3f505e;
+}
+
+/* the dialog styles */
+.ticket_title_background {
+  background: #3f505e;
+  /* background: #618AAB; */
+  color: white;
+  display: flex;
+  justify-content: center;
+}
+
+.terminate_ticket {
+  background: #e28743;
+  color: white;
+}
+
+.ticket_background {
+  background: #dce4eb;
+}
+
+.closeWhite {
+  color: white;
+}
+
+.comments_color {
+  color: #3f505e;
+  font-size: 24px;
+}
+</style>
